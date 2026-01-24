ff6fc20ac99b8c1e561247138b194e6b783d46d2
Nikolai <degnikolaj@gmail.com>
2026-01-24 21:17:08 +0100

    add consultationn 9

---
+++ consultation_9/my-redux-app/src/App.tsx
@@ -0,0 +1,15 @@
+
+import type { JSX } from 'react'
+import './App.css'
+import Counter from './features/counter/Counter'
+
+function App(): JSX.Element{
+  
+  return (
+   <div>
+    <Counter/>
+   </div>
+  )
+}
+
+export default App
